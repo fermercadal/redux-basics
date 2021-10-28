5cd35a62ba6c24877fa039d24d5e6262b89741e6
Fernando Mercadal <fermercadal@outlook.com>
2021-10-28 20:18:20 -0300

    Adding Redux basics

--- redux-demo.js
+++ redux-demo.js
@@ -1,2 +1,33 @@
 const redux = require("redux");
 
+// Reducer Function
+const counterReducer = (state = { counter: 0 }, action) => {
+  if(action.type === 'increment') {
+    return {
+      counter : state.counter + 1,
+    }
+  }
+
+  if(action.type === 'decrement') {
+    return {
+      counter : state.counter - 1,
+    }
+  }
+
+  return state;
+};
+
+// Redux Store
+const store = redux.createStore(counterReducer);
+
+// Subscription Function
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+// Dispatch Actions
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
